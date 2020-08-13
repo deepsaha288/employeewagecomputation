2af1914963d838fd26ad437d6408b0d774f3c19f
Deep Saha <deepsaha288@yahoo.com>
2020-08-13 18:44:42 +0530

    my 4th shell program done

---
+++ caseProblem.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+empRatePerHours=20
+isPresent=1
+isPart=2
+randomCheck=$(( RANDOM%3 ))
+case "$randomCheck" in
+   "$isPresent")
+         empHrs=8 ;;
+         echo "full time" ;;
+   "$isPart")
+         empHrs=4;;
+         echo "part time" ;;
+   "*")
+      echo"absent" ;;
+esac
+salary=$(( $empRatePerHours * $empHrs ))
+echo "salary" $salary
+
